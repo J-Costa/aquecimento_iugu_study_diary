2821f50982ca05980403c564a80bff221b426b17
Josue Costa <juncosta@gmail.com>
2021-03-03 19:49:32 -0300

    Extração de métodos para classe

--- study_diary.rb
+++ study_diary.rb
@@ -1,3 +1,4 @@
+require_relative 'study_item'
 def clear
     system('clear')
 end
@@ -26,46 +27,21 @@ def menu
     gets.to_i
 end
 
-def register_study_item
-    print 'Digite o título do seu item de estudo: '
-    title = gets.chomp
-    print 'Digite a categoria do seu item de estudo: '
-    category = gets.chomp
-    puts "Item '#{title}' da categoria '#{category}' cadastrado com sucesso!"
-    { title: title, category: category }
-end
-
-def print_items(collection)
-    collection.each_with_index do |item, index|
-        puts "##{index + 1} - #{item[:title]} - #{item[:category]}"
-    end
-    puts 'Nenhum item cadastrado' if collection.empty?
-end
-
-def search_items(collection)
-    print 'Digite uma palavra para procurar: '
-    term = gets.chomp
-    found_items = collection.filter do |item|
-        item[:title].include?(term)
-    end
-    print_items(found_items)
-    puts 'Nenhum item encontrado' if collection.empty?
-end
-
 clear
 puts welcome
-study_items = []
 option = menu
 
 loop do
     clear
     case option
     when 1
-        study_items << register_study_item
+        Study_item.register
     when 2
-        print_items(study_items)
+        Study_item.all
     when 3
-        search_items(study_items)
+        print "Digite o termo para buscar na categoria ou título: "
+        query = gets.chomp
+        Study_item.search(query)
     when 4
         clear
         puts 'Obrigado por usar o Diário de Estudos'
--- study_item.rb
+++ study_item.rb
@@ -1,8 +1,47 @@
-def Study_item 
-    attr_reader :title, :category
+class Study_item 
+    attr_reader :id, :title, :category
+    
+    @@next_id = 0
+    @@study_items = []
 
     def initialize(title, category)
+        @@next_id +=1
+
+        @id = @@next_id
         @title = title
         @category = category
+
+        @@study_items << self
+    end
+
+    def self.register
+        print 'Digite o título do seu item de estudo: '
+        title = gets.chomp
+        print 'Digite a categoria do seu item de estudo: '
+        category = gets.chomp
+        puts "Item '#{title}' da categoria '#{category}' cadastrado com sucesso!"
+        new(title, category)
+    end
+
+    def self.search(query)
+        found = []
+        @@study_items.each do |item|
+            if item.title.include?(query) || item.category.include?(query)
+                found << item
+            end
+        end
+        if found.empty?
+            puts "Item não encontrado"
+        else
+            puts found
+        end
+    end
+    
+    def self.all 
+        puts @@study_items
+    end
+
+    def to_s 
+        "##{@id} - #{@title} - #{@category}"
     end
 end
