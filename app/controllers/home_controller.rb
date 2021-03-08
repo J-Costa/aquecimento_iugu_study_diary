d96612f0c80f10b2bc7077ec4b3db1767beb6db5
Josue Costa <juncosta@gmail.com>
2021-03-08 19:56:33 -0300

    Estudo Rails: migrations e view de item

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,9 @@
+class HomeController < ApplicationController
+
+    def index
+        # codigo, acessar banco de dados etc
+        # render 'lista' => por padrao o Rails já vai procurar essa view
+        
+        @study_items = StudyItem.all
+    end
+end
